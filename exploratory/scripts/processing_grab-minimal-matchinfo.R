224316ffd9e408eaea8597d27cf2eb9b70d74ce3
Hoyd <45100697+hoyd44@users.noreply.github.com>
2024-05-15 16:11:35 -0400

    Remove unused scripts for publication

--- exploratory/scripts/processing_grab-minimal-matchinfo.R
+++
@@ -1,8 +0,0 @@
-matched.dat <- read.csv("T:/Labs/Presley/FITNESS-lung/Microbiome- Rebecca/clinical-OC-matched_resolved.csv",
-                        stringsAsFactors = F)
-microbe.key <- matched.dat %>%
-  select(id, patient.id,redcap_event_name, sequence.id) %>%
-  distinct()
-
-write.csv(microbe.key, "T:/Labs/Presley/FITNESS-lung/Microbiome- Rebecca/key_fitness-microbe-clin.csv",
-          row.names = F)
